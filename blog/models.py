5729bd8e98109602834a75a674833c3dd1cbd0c5
genderal <eng.mhassan88@gmail.com>
2018-04-22 08:14:15 +0200

    i created blog model but i will re arrange in further commits

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
+class Blog(models.Model):
+    my_status=models.CharField(max_length=1000)
+    image=models.ImageField(upload_to= 'blog_images/')
+    title=models.CharField(max_length=255,blank=True)
+    pub_date=models.DateTimeField(default=datetime.now(),blank=True)
 
-# Create your models here.
+
+
+    # Create your models here.
