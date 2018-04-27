60e5c8ee48788560038a7323bc28f594a1937541
genderal <eng.mhassan88@gmail.com>
2018-04-27 14:20:58 +0200

    don't forget to import first and add name for each path

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,5 @@
-
 from django.urls import path
-from django.conf import settings
-
+from . import views
 urlpatterns = [
-    path('',blog.views.home),
+    path('',views.allblogs,name='allblogs',),
 ]
