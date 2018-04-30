b0e31e309d2a2057ddbdcd5660614f66577efe97
genderal <eng.mhassan88@gmail.com>
2018-04-30 16:30:06 +0200

    added function to blog models to make date without time and make consize status

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,13 @@ class Blog(models.Model):
     my_status=models.TextField()
     image=models.ImageField(upload_to= 'blog_images/')
 
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.my_status[:100]
+
+    def pub_date_simple(self):
+        return self.pub_date.strftime('%b %e %Y')
+
     # Create your models here.
